004569f87e746c644e2d4707e58dc0842a0fa8d3
yosr <46849964+zoroymoi@users.noreply.github.com>
2021-01-30 22:47:05 +0100

    campaign stops

--- race stats/bernie.py
+++ race stats/bernie.py
@@ -3,3 +3,4 @@ bernie = {'name':'Bernie','origin':'Brooklyn,NY','age':'78',}
 
 StatesWon=['IL','OR','NC','SC','MO','HI','AL']
 DelegateCount =['IA','CA','MS','CO']
+CampaignStops= ['Des Moines','Minneapolis', 'Sioux Falls','Baton Rouge']
